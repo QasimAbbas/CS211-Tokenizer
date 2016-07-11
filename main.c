4f7c786d5425adaadd1a95e9b7cd1879dcd57354
Qasim Abbas <qasimabbas52@gmail.com>
2016-07-11 00:57:07 -0400

    Working out Tokenizer a bit more

--- main.c
+++ main.c
@@ -13,16 +13,17 @@
 int main(int argc, char ** argv){
 	/*check the argument*/
 	//printf("Running the file for the first time");
-
+    
 	printf("Reading the FILE:  %s \n", argv[1]);
 	
-	TKCreate(argv[1]);		
+	TKCreate(argv[1]);
 
 	/*call TKPrint() with the returned object and print out the result + error message*/
 
 
 	/*destroy all the dynamically allocated memory*/
 	dummyFunc();
-
+    
+    
 	return 0;
 }
--- tokenizer.c
+++ tokenizer.c
@@ -5,6 +5,9 @@
  */
 
 #include "tokenizer.h"
+#include <string.h>
+#include <ctype.h>
+
 /* change the 'return NULL' after you finished the code */
 /* functions specifications are in tokenizer.h */
 
@@ -16,17 +19,36 @@ TokenizerT *TKCreate(char * ts){
 	/* fill in your code here */
 
 	printf("Into TKCreate with the FILE: %s \n", ts);
-	Node *head = (Node *)malloc(sizeof(Node));
-	Node *tail = (Node *)malloc(sizeof(Node));
+//	Node *head = (Node *)malloc(sizeof(Node));
+//	Node *tail = (Node *)malloc(sizeof(Node));
+
+    char* str = (char *)malloc(sizeof(char));
+    
+    FILE *file;
+    file = fopen(ts, "r");
+    
+    while(fgets(str, 50, file)){
+        char *tmp = str;
+        
+        while(*tmp != '\0'){
+            
+            char *nexttoken = TKGetNextToken(tmp);
+            
+            printf("THE NEXT TOKEN: %s\n", nexttoken);
+            tmp++;
+        
+        }
+    }
+
 /* you might need more than a few helper functions */
 
 /* SAMPLE LOGIC:
  * traverse through the file line by line
  * in each line, repeatedly call TKGetNextToken() until the end of line
- * store the result from TKGetNextToken() to your structure 
- * interpret the informations and store the interpretation in your structure 
+ * store the result from TKGetNextToken() to your structure
+ * interpret the informations and store the interpretation in your structure
 */
-	
+
 	return NULL;
 }
 
@@ -36,7 +58,19 @@ void TKDestroy( TokenizerT * tk ) {
 
 char *TKGetNextToken( char * start ) {
     	/* fill in your code here */
-	return NULL;
+    char * ptr = start;
+    char * tmp = start;
+    
+    int index = 0;
+    
+    while(*ptr != ' '){
+        ptr++;
+        index++;
+    }
+    char *token = (char *)malloc(index+1);
+    strncpy(token, tmp, index++);
+    
+	return token;
 }
 
 void TKPrint(TokenizerT *tk){
